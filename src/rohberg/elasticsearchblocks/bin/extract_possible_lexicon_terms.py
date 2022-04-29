07c8de2980ff832ead4afb835fa73f1391ecbee3
ksuess <k.suess@rohberg.ch>
2022-04-29 19:12:44 +0200

    Create extract_possible_lexicon_terms.py

---
+++ src/rohberg/elasticsearchblocks/bin/extract_possible_lexicon_terms.py
@@ -0,0 +1,28 @@
+# https://kb.objectrocket.com/elasticsearch/export-elasticsearch-documents-as-csv-html-and-json-files-in-python-using-pandas-348
+"""
+Run with
+python /Users/katjasuss/Plone/igib/deployigib/work/zope/src/rohberg.elasticsearchblocks/src/rohberg/elasticsearchblocks/bin/extract_possible_lexicon_terms.py
+"""
+
+import re
+
+es_export_file = 'export_elasticsearch_data.csv'
+possible_lexicon_terms_file = "extract_possible_lexicon_terms.txt"
+
+regexp = r"[A-ZÄÖÜ][a-zA-Z_äöüÄÖÜß]+"
+
+
+def main():
+    with open(es_export_file, 'r') as inputfile:
+        data = inputfile.read()
+        # print(data[:300])
+        words = re.findall(regexp, data)
+        print("set(words)", set(words))
+        print("words", words)
+        with open(possible_lexicon_terms_file, 'w') as exportfile:
+            for word in set(words):
+                exportfile.write(f"{word}\n")
+
+
+if __name__ == "__main__":
+    main()
